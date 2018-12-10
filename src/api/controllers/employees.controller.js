648825fe5bbc0dd07b38999a89cc1a97bcd9067e
Alexander Belenkov <belenkov.alexander@gmail.com>
2018-12-10 15:32:28 +0300

    Add extra options for get all employees endpoint

--- src/api/controllers/employees.controller.js
+++ src/api/controllers/employees.controller.js
@@ -1,4 +1,5 @@
 const status = require('http-status');
+const _ = require('lodash');
 const {
   getAllEmployees,
   getEmployeeDetails,
@@ -11,10 +12,29 @@ const CONFIG = require('../../config');
 
 module.exports.getAll = async (req, res) => {
   const { token } = decodeJWT(req.headers.authorization);
+  const position = req.query.position || '';
+  const location = req.query.location || '';
+
   const [err, data] = await toWithCache(
-    getAllEmployees(token, CONFIG.smg.host).then(response => response.data.Profiles),
+    getAllEmployees(token, CONFIG.smg.host)
+      .then(response => response.data.Profiles)
+      // eslint-disable-next-line no-shadow
+      .then(data => {
+        if (position) {
+          return _.filter(data, profile => profile.Position.toLowerCase().indexOf(position) !== -1);
+        }
+        return data;
+      })
+      // eslint-disable-next-line no-shadow
+      .then(data => {
+        if (location) {
+          return _.filter(data, profile => profile.Room.toLowerCase().indexOf(location) !== -1);
+        }
+        return data;
+      }),
     req.originalUrl || req.url
   );
+
   if (err) return ReE(res, err, status.BAD_REQUEST);
   return ReS(res, data);
 };
@@ -22,12 +42,14 @@ module.exports.getAll = async (req, res) => {
 module.exports.getAllWithShortModel = async (req, res) => {
   const { token } = decodeJWT(req.headers.authorization);
   const initialRequest = req.query.initialRequest || true;
+
   const [err, data] = await toWithCache(
     getEmployeeShortInfo(initialRequest, token, CONFIG.smg.host).then(
       response => response.data.Profiles
     ),
     req.originalUrl || req.url
   );
+
   if (err) return ReE(res, err, status.BAD_REQUEST);
   return ReS(res, data);
 };
@@ -35,11 +57,12 @@ module.exports.getAllWithShortModel = async (req, res) => {
 module.exports.getEmployeeByProfileId = async (req, res) => {
   const { token } = decodeJWT(req.headers.authorization);
   const { id } = req.params;
+
   const [err, data] = await toWithCache(
     getEmployeeDetails(id, token, CONFIG.smg.host).then(response => response.data.Profile),
     req.originalUrl || req.url
   );
-  console.log(typeof data);
+
   if (err) return ReE(res, err, status.BAD_REQUEST);
   return ReS(res, data);
 };
@@ -47,10 +70,12 @@ module.exports.getEmployeeByProfileId = async (req, res) => {
 module.exports.getAllEmployeesByDeptId = async (req, res) => {
   const { token } = decodeJWT(req.headers.authorization);
   const { id } = req.params;
+
   const [err, data] = await toWithCache(
     getEmployeesByDeptId(id, token, CONFIG.smg.host).then(response => response.data.Profiles),
     req.originalUrl || req.url
   );
+
   if (err) return ReE(res, err, status.BAD_REQUEST);
   return ReS(res, data);
 };
